d20f12cf04c6d2cd2e1a3ed157e6c661a2daa6b9
JaswinderSinghdigwa <findjassi121212@gmail.com>
2022-01-16 13:23:04 +0530

    Refactor : reafctor code of Sign in Pages

--- src/Pages/Signing/Signin.js
+++ src/Pages/Signing/Signin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Signin.css';
+import { login } from '../../Service/service'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 const emailRegex = /^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$/;
@@ -7,44 +8,62 @@ const passRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 
 
 export default function Signin() {
-    const [email,setEmail] = React.useState("");
-    const [password,setPassword] = React.useState("");
-    const [regexobj, setregexobj] = React.useState({emailborder:false, passwordborder:false})
-    const [regexhelpertext,setregexhelpertext]=React.useState({emailhelpertext:"",passwordhelpertext:""})
-        
-    const takeEmail = (e)=>{
-       setEmail(e.target.value);
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [regexobj, setregexobj] = React.useState({ emailborder: false, passwordborder: false })
+    const [regexhelpertext, setregexhelpertext] = React.useState({ emailhelpertext: "", passwordhelpertext: "" })
+
+    const Email = (e) => {
+        setEmail(e.target.value);
     }
-    const takePassword = (e) =>{
+    const Password = (e) => {
         setPassword(e.target.value);
     }
-    const submit = ()=>{
-        if(email===""&& password===""){
-            setregexobj({...regexobj,emailborder:true,passwordborder:true})
-            setregexhelpertext({...regexhelpertext,emailhelpertext:"Enter a correct email",passwordhelpertext:"Enter a correct password"})
+    const submit = async() => {
+        if (email === "" && password === "") {
+            setregexobj({ ...regexobj, emailborder: true, passwordborder: true })
+            setregexhelpertext({ ...regexhelpertext, emailhelpertext: "Enter a correct email", passwordhelpertext: "Enter a correct password" })
         }
-        else{
-            let emailtest=emailRegex.test(email)
-            let passwordtest=passRegex.test(password)
-            if(emailtest){
-                setregexobj(regexobj=>({...regexobj,emailborder:false}))
-                setregexhelpertext({...regexhelpertext,emailhelpertext:""})
+        else {
+            let emailtest = emailRegex.test(email)
+            let passwordtest = passRegex.test(password)
+            if (emailtest) {
+                setregexobj(regexobj => ({ ...regexobj, emailborder: false }))
+                setregexhelpertext({ ...regexhelpertext, emailhelpertext: "" })
             }
-            else{
-                setregexobj(regexobj=>({...regexobj,emailborder:true}))
-                setregexhelpertext({emailhelpertext:"Enter a correct Email"})
+            else {
+                setregexobj(regexobj => ({ ...regexobj, emailborder: true }))
+                setregexhelpertext({ emailhelpertext: "Enter a correct Email" })
             }
-            if(passwordtest){
-                setregexobj(regexobj=>({...regexobj,passwordborder:false}))
-                setregexhelpertext({...regexhelpertext,passwordhelpertext:""})
+            if (passwordtest) {
+                setregexobj(regexobj => ({ ...regexobj, passwordborder: false }))
+                setregexhelpertext({ ...regexhelpertext, passwordhelpertext: "" })
             }
-            else{
-                setregexobj(regexobj=>({...regexobj,passwordborder:true}))
-                setregexhelpertext({...regexhelpertext,passwordhelpertext:"Enter a correct password"})
+            else {
+                setregexobj(regexobj => ({ ...regexobj, passwordborder: true }))
+                setregexhelpertext({ ...regexhelpertext, passwordhelpertext: "Enter a correct password" })
+            }
+            if (emailtest === true && passwordtest === true) {
+                let obj = {
+                    "email": email,
+                    "password": password
+                }
+               let res = await login(obj)
+               try{
+                if(!res){
+                    console.log(res)
+                }else if(res){
+                    console.log(res)
+                }
+                    console.log(obj);
+               }
+               catch(error){
+                   console.log(error)
+               }    
             }
         }
+    }   
 
-    }
     return (
         <div className="Box-class">
             <div className="form-class">
@@ -63,17 +82,17 @@ export default function Signin() {
                     <h5>Use Your Account Here</h5>
                 </div>
                 <div className='email1'>
-                    <TextField id="Email" onChange={takeEmail} helperText={regexhelpertext.emailhelpertext} error={regexobj.emailborder} label="Email or Phone" size='small' variant="outlined" />
+                    <TextField id="Email" onChange={Email} helperText={regexhelpertext.emailhelpertext} error={regexobj.emailborder} label="Email or Phone" size='small' variant="outlined" />
                 </div>
                 <div className="password">
-                <TextField id="password" onChange={takePassword} helperText={regexhelpertext.passwordhelpertext} error={regexobj.passwordborder} label="Password" size='small' variant="outlined" />
+                    <TextField id="password" onChange={Password} helperText={regexhelpertext.passwordhelpertext} error={regexobj.passwordborder} label="Password" size='small' variant="outlined" />
                 </div>
                 <div className='textletter'>
-                    Not your computer? Use Guest mode to sign in privately.</div>
+                    Use to Login in privately</div>
                 <div className='account'>
-                    <h2 className='accountcolor'><a id = "GFG"href='SignIn.css'>Create account</a></h2>
+                    <h2 className='accountcolor'><a id="GFG" href='SignIn.css'> Create New Account</a></h2>
                     <div className='btm'>
-                        <Button onClick={submit} variant="contained">Next</Button>
+                        <Button onClick={submit} variant="contained">login</Button>
                     </div>
                 </div>
             </div>
